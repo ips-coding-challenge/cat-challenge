890a86ed16b80bd1adc0e01dcae475a3bdb27969
myself <android.programmation@laposte.net>
2020-07-21 17:49:07 +0200

    Add Purge css and delete yarn lock

--- client/postcss.config.js
+++ client/postcss.config.js
@@ -1,6 +1,14 @@
+const purgecss = require("@fullhuman/postcss-purgecss")({
+  content: ["./src/**/*.jsx", "./src/**/*.js", "./public/index.html"],
+  css: ["./src/assets/css/main.css"],
+  // Include any special characters you're using in this regular expression
+  defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+});
+
 module.exports = {
   plugins: [
     require("tailwindcss")("./tailwind.config.js"),
     require("autoprefixer"),
+    ...(process.env.NODE_ENV === "production" ? [purgecss] : []),
   ],
 };
--- client/src/index.js
+++ client/src/index.js
@@ -9,6 +9,7 @@ import { createStore } from "redux";
 import { Provider } from "react-redux";
 import reducer from "./store/store";
 import SimpleReactLightbox from "simple-react-lightbox";
+import * as serviceWorker from "./serviceWorker";
 
 const store = createStore(reducer);
 
@@ -22,3 +23,8 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
