0630b690bf7fef5d1a55841eeb01bc7da50247bc
myself <android.programmation@laposte.net>
2020-07-21 17:02:08 +0200

    Add express static to serve the index.html from react

--- index.js
+++ index.js
@@ -142,6 +142,15 @@ app.post("/breeds", async (req, res) => {
   }
 });
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("web/build"));
+
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "web", "build", "index.html"));
+  });
+}
+
 app.listen(5000, () => {
   console.log(`Listening on port 5000`);
 });
